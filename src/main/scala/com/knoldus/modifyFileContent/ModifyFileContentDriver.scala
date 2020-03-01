ed158e0195fc5ba6f1483516b9f43bf68b99565b
mansi7babbar <mansi7babbar@gmail.com>
2020-03-01 11:06:25 +0530

    added relative path of directory

--- src/main/scala/com/knoldus/modifyFileContent/ModifyFileContentDriver.scala
+++ src/main/scala/com/knoldus/modifyFileContent/ModifyFileContentDriver.scala
@@ -5,10 +5,10 @@ import java.io.File
 import com.knoldus.fileOperations.{CreateFile, ListOfFiles, ReadFile}
 
 object ModifyFileContentDriver {
-  val directoryName = "/home/knoldus/IdeaProjects/scala-assignment-eighth/src/main/scala/com/knoldus/my-files"
+  val directoryName = "./src/main/scala/com/knoldus/my-files"
   val listOfFiles: List[File] = ListOfFiles.getListOfFiles(directoryName)
   for (file <- listOfFiles) {
-    val newDirectoryName = "/home/knoldus/IdeaProjects/scala-assignment-eighth/src/main/scala/com/knoldus/modifyFileContent/modified-files"
+    val newDirectoryName = "./src/main/scala/com/knoldus/modifyFileContent/modified-files"
     val fileContents = ReadFile.readFromFile(file)
     val newFile = CreateFile.createNewFile(file.getName, newDirectoryName)
     val updatedFile = WriteFile.writeInFile(newFile, fileContents)
--- src/main/scala/com/knoldus/uniqueWordsCount/UniqueWordsCountDriver.scala
+++ src/main/scala/com/knoldus/uniqueWordsCount/UniqueWordsCountDriver.scala
@@ -5,10 +5,10 @@ import java.io.File
 import com.knoldus.fileOperations.{CreateFile, ListOfFiles, ReadFile}
 
 object UniqueWordsCountDriver {
-  val directoryName = "/home/knoldus/IdeaProjects/scala-assignment-eighth/src/main/scala/com/knoldus/my-files"
+  val directoryName = "./src/main/scala/com/knoldus/my-files"
   val listOfFiles: List[File] = ListOfFiles.getListOfFiles(directoryName)
   for (file <- listOfFiles) {
-    val newDirectoryName = "/home/knoldus/IdeaProjects/scala-assignment-eighth/src/main/scala/com/knoldus/uniqueWordsCount/words-count"
+    val newDirectoryName = "./src/main/scala/com/knoldus/uniqueWordsCount/words-count"
     val fileContents = ReadFile.readFromFile(file)
     val newFile = CreateFile.createNewFile(file.getName, newDirectoryName)
     val wordCount = UniqueWordsCount.getUniqueWordsCount(fileContents).toString()
